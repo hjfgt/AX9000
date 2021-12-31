29508fcd9503336fcdd29b500a64aa4ab46a4470
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2021-12-31 20:08:32 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,23 @@
+##添加自己的插件库
+rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/luci-app-syncdial
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lean/luci-app-jd-dailybonus
+rm -rf ./package/lean/luci-lib-docker
+rm -rf ./package/lean/luci-app-pushbot
+rm -rf ./package/lean/luci-app-netdata
+rm -rf ./package/lean/aliyundrive-webdav
+rm -rf ./package/lean/luci-app-aliyundrive-webdav
+rm -rf ./package/lean/go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
+rm -rf ./package/lean/luci-app-easymesh
+rm -rf ./package/lean/luci-app-cpufreq
+rm -rf ./package/lean/luci-app-kodexplorer
+  
+# 添加插件库
+echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
+#echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+#echo -e "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#echo -e "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
+
+svn checkout  https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash ./package/lean/luci-app-openclash
