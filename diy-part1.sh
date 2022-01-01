2fe0d745387f0faecd96eca300fdb9513e8fa48c
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2022-01-01 12:13:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,16 +1,10 @@
 ##添加自己的插件库
-rm -rf ./package/lean/k3screenctrl
-rm -rf ./package/lean/luci-app-syncdial
+
 rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/lean/luci-lib-docker
-rm -rf ./package/lean/luci-app-pushbot
-rm -rf ./package/lean/luci-app-netdata
 rm -rf ./package/lean/aliyundrive-webdav
 rm -rf ./package/lean/luci-app-aliyundrive-webdav
 rm -rf ./package/lean/go-aliyundrive-webdav
 rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
-rm -rf ./package/lean/luci-app-easymesh
 rm -rf ./package/lean/luci-app-cpufreq
 rm -rf ./package/lean/luci-app-kodexplorer
   
