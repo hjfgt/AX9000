54bfa28b36a53f501a1036c7a4823d97debb9469
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2022-01-01 13:30:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,10 +3,8 @@
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/aliyundrive-webdav
 rm -rf ./package/lean/luci-app-aliyundrive-webdav
-rm -rf ./package/lean/go-aliyundrive-webdav
 rm -rf ./package/lean/luci-app-go-aliyundrive-webdav
 rm -rf ./package/lean/luci-app-cpufreq
-rm -rf ./package/lean/luci-app-kodexplorer
   
 # 添加插件库
 echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
