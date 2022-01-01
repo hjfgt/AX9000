0e404a9ed7fbc1609dca619875cbb45cb2ea2495
hongcz1104 <83286089+hongcz1104@users.noreply.github.com>
2022-01-01 15:42:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,4 @@
-##添加自己的插件库
-
+# 删除原插件
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/aliyundrive-webdav
 rm -rf ./package/lean/luci-app-aliyundrive-webdav
@@ -8,4 +7,3 @@ rm -rf ./package/lean/luci-app-cpufreq
   
 # 添加插件库
 echo -e "\nsrc-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
-
